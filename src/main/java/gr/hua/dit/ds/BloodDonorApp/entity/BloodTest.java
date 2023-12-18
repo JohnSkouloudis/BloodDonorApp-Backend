3b9bb8a95d5b35ddbcc6076e925c2b459947a73c
John skoul <johnskoul56@gmail.com>
2023-12-18 21:04:01 +0200

    Database

---
+++ src/main/java/gr/hua/dit/ds/BloodDonorApp/entity/BloodTest.java
@@ -0,0 +1,157 @@
+package gr.hua.dit.ds.BloodDonorApp.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class BloodTest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer id;
+
+    @Column
+    private Integer height;
+
+    @Column
+    private Integer weight;
+
+    @Column
+    private Integer rhesusFactor;
+
+    @Column
+    private Integer bloodPressure;
+
+    @Column
+    private Integer whiteBloodCellCount;
+
+    @Column
+    private Integer redBloodCellCount;
+
+    @Column
+    private String dateOfTest;
+
+    @Column
+    private String hospitalName;
+
+    public BloodTest() {
+    }
+
+    public BloodTest(Integer id,
+                     Integer height,
+                     Integer weight,
+                     Integer rhesusFactor,
+                     Integer bloodPressure,
+                     Integer whiteBloodCellCount,
+                     Integer redBloodCellCount,
+                     String dateOfTest,
+                     String hospitalName) {
+        this.id = id;
+        this.height = height;
+        this.weight = weight;
+        this.rhesusFactor = rhesusFactor;
+        this.bloodPressure = bloodPressure;
+        this.whiteBloodCellCount = whiteBloodCellCount;
+        this.redBloodCellCount = redBloodCellCount;
+        this.dateOfTest = dateOfTest;
+        this.hospitalName = hospitalName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public Integer getRhesusFactor() {
+        return rhesusFactor;
+    }
+
+    public void setRhesusFactor(Integer rhesusFactor) {
+        this.rhesusFactor = rhesusFactor;
+    }
+
+    public Integer getBloodPressure() {
+        return bloodPressure;
+    }
+
+    public void setBloodPressure(Integer bloodPressure) {
+        this.bloodPressure = bloodPressure;
+    }
+
+    public Integer getWhiteBloodCellCount() {
+        return whiteBloodCellCount;
+    }
+
+    public void setWhiteBloodCellCount(Integer whiteBloodCellCount) {
+        this.whiteBloodCellCount = whiteBloodCellCount;
+    }
+
+    public Integer getRedBloodCellCount() {
+        return redBloodCellCount;
+    }
+
+    public void setRedBloodCellCount(Integer redBloodCellCount) {
+        this.redBloodCellCount = redBloodCellCount;
+    }
+
+    public String getDateOfTest() {
+        return dateOfTest;
+    }
+
+    public void setDateOfTest(String dateOfTest) {
+        this.dateOfTest = dateOfTest;
+    }
+
+    public String getHospitalName() {
+        return hospitalName;
+    }
+
+    public void setHospitalName(String hospitalName) {
+        this.hospitalName = hospitalName;
+    }
+
+    @Override
+    public String toString() {
+        return "BloodTest{" +
+                "id=" + id +
+                ", height=" + height +
+                ", weight=" + weight +
+                ", rhesusFactor=" + rhesusFactor +
+                ", bloodPressure=" + bloodPressure +
+                ", whiteBloodCellCount=" + whiteBloodCellCount +
+                ", redBloodCellCount=" + redBloodCellCount +
+                ", dateOfTest='" + dateOfTest + '\'' +
+                ", hospitalName='" + hospitalName + '\'' +
+                '}';
+    }
+
+    @OneToOne(mappedBy = "bloodTest", optional = false)
+    private Application application;
+
+    public Application getApplication() {
+        return application;
+    }
+
+    public void setApplication(Application application) {
+        this.application = application;
+    }
+}
