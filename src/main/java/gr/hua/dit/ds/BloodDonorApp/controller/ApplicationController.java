0a686bde225b411252a92984d9268528958b8127
Panayiotis Perdios <panos.perdios@gmail.com>
2023-12-27 15:35:30 +0200

    made applicationController

---
+++ src/main/java/gr/hua/dit/ds/BloodDonorApp/controller/ApplicationController.java
@@ -0,0 +1,54 @@
+package gr.hua.dit.ds.BloodDonorApp.controller;
+
+import gr.hua.dit.ds.BloodDonorApp.entity.Application;
+import gr.hua.dit.ds.BloodDonorApp.service.ApplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("application")
+public class ApplicationController {
+    private ApplicationService applicationService;
+
+    @Autowired
+    public ApplicationController(ApplicationService applicationService) {
+        this.applicationService = applicationService;
+    }
+
+    @GetMapping("/all")
+    @ResponseBody
+    public List<Application> getApplications() {
+        List<Application> applications = applicationService.getApplications();
+        return applications;
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<Application> saveApplication(@RequestBody Application application){
+        Application savedApplication = applicationService.saveApplication(application);
+        return ResponseEntity.ok(savedApplication);
+    }
+
+    @PostMapping("/{applicationId}/approve")
+    public ResponseEntity<Application> approveApplication(@PathVariable Integer applicationId){
+        Application approvedApplication = applicationService.approveApplication(applicationId);
+        if (approvedApplication != null) {
+            return ResponseEntity.ok(approvedApplication);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/{applicationId}/reject")
+    public ResponseEntity<Application> rejectApplication(@PathVariable Integer applicationId) {
+        Application rejectedApplication = applicationService.rejectApplication(applicationId);
+        if (rejectedApplication != null) {
+            return ResponseEntity.ok(rejectedApplication);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+}
