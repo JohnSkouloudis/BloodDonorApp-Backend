56041091647dfe4f67e5d707ff9620f4d41fac39
Panayiotis Perdios <panos.perdios@gmail.com>
2024-01-02 15:13:04 +0200

    removed BloodTestController and all functionallities are goin to ApplicationController

--- src/main/java/gr/hua/dit/ds/BloodDonorApp/controller/ApplicationController.java
+++ src/main/java/gr/hua/dit/ds/BloodDonorApp/controller/ApplicationController.java
@@ -1,7 +1,9 @@
 package gr.hua.dit.ds.BloodDonorApp.controller;
 
 import gr.hua.dit.ds.BloodDonorApp.entity.Application;
+import gr.hua.dit.ds.BloodDonorApp.entity.BloodTest;
 import gr.hua.dit.ds.BloodDonorApp.service.ApplicationService;
+import gr.hua.dit.ds.BloodDonorApp.service.BloodTestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +15,8 @@ import java.util.List;
 @RequestMapping("application")
 public class ApplicationController {
     private ApplicationService applicationService;
+    private BloodTestService bloodTestService;
 
-    @Autowired
-    public ApplicationController(ApplicationService applicationService) {
-        this.applicationService = applicationService;
-    }
 
     @GetMapping("/all")
     @ResponseBody
@@ -65,5 +64,33 @@ public class ApplicationController {
 //            return ResponseEntity.notFound().build();
 //        }
 //    }
+
+    @GetMapping("/bloodtest/all")
+    @ResponseBody
+    public List<BloodTest> getBloodTests(){
+        List<BloodTest> bloodTests = bloodTestService.getBloodTests();
+        return bloodTests;
+
+    }
+
+    @GetMapping("/bloodtest/{bloodTestId}")
+    @ResponseBody
+    public BloodTest getBloodTest(@PathVariable Integer bloodTestId){
+        BloodTest bloodTest = bloodTestService.getBloodTest(bloodTestId);
+        return bloodTest;
+    }
+
+    @DeleteMapping("/bloodtest/{bloodTestId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<String> deleteBloodTest(@PathVariable Integer bloodTestId){
+        bloodTestService.deleteBloodTest(bloodTestId);
+        return ResponseEntity.ok("BloodTest with ID " + bloodTestId + " deleted successfully.");
+    }
+
+    @PostMapping("/bloodtest/new")
+    public ResponseEntity<BloodTest> saveBloodTest(@RequestBody BloodTest bloodTest){
+        BloodTest savedBloodTest = bloodTestService.saveBloodTest(bloodTest);
+        return ResponseEntity.ok(savedBloodTest);
+    }
 
 }
