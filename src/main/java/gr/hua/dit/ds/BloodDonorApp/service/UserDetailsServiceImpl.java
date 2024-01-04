bf5090fc15b3b53cb7b14d837b7bcf2c536364bf
Panayiotis Perdios <panos.perdios@gmail.com>
2024-01-04 16:06:58 +0200

    changed a few controllers and services

--- src/main/java/gr/hua/dit/ds/BloodDonorApp/service/UserDetailsServiceImpl.java
+++ src/main/java/gr/hua/dit/ds/BloodDonorApp/service/UserDetailsServiceImpl.java
@@ -1,40 +1,20 @@
 package gr.hua.dit.ds.BloodDonorApp.service;
 
-import gr.hua.dit.ds.BloodDonorApp.entity.Role;
 import gr.hua.dit.ds.BloodDonorApp.entity.User;
-import gr.hua.dit.ds.BloodDonorApp.repository.RoleRepository;
 import gr.hua.dit.ds.BloodDonorApp.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.HashSet;
-import java.util.Set;
 
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private RoleRepository roleRepository;
-
-    @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
-
-    @Autowired
-    public UserDetailsServiceImpl(BCryptPasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
-    }
-
-
+    UserRepository userRepository;
 
     @Override
     @Transactional
@@ -43,27 +23,5 @@ public class UserDetailsServiceImpl implements UserDetailsService {
                 .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
 
         return UserDetailsImpl.build(user);
-    }
-
-    @Transactional
-    public Integer saveUser(User user){
-        String password= user.getPassword();
-        String encodedPassword = passwordEncoder.encode(password);
-        user.setPassword(encodedPassword);
-
-        Role role = roleRepository.findByName("ROLE_USER")
-                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-        Set<Role> roles = new HashSet<>();
-        roles.add(role);
-        user.setRoles(roles);
-
-        userRepository.save(user);
-        return user.getId();
-    }
-
-    @Transactional
-    public Integer updateUer(User user) {
-        user = userRepository.save(user);
-        return user.getId();
     }
 }
