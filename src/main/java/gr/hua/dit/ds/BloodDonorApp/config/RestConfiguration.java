a83c6190f6cc2cee4c38205459430222160a8634
John skoul <johnskoul56@gmail.com>
2024-01-22 20:22:56 +0200

    added hidden to restcontrollers and now using the custome restcontrollers from the repositories

---
+++ src/main/java/gr/hua/dit/ds/BloodDonorApp/config/RestConfiguration.java
@@ -0,0 +1,21 @@
+package gr.hua.dit.ds.BloodDonorApp.config;
+
+import gr.hua.dit.ds.BloodDonorApp.entity.Hospital;
+import gr.hua.dit.ds.BloodDonorApp.entity.User;
+import gr.hua.dit.ds.BloodDonorApp.entity.Application;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class RestConfiguration implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(
+            RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(Hospital.class);
+        config.exposeIdsFor(User.class);
+        config.exposeIdsFor(Application.class);
+    }
+}
---
+++ src/main/java/gr/hua/dit/ds/BloodDonorApp/service/InitialDataService.java
@@ -0,0 +1,154 @@
+package gr.hua.dit.ds.BloodDonorApp.service;
+
+
+import gr.hua.dit.ds.BloodDonorApp.entity.Application;
+import gr.hua.dit.ds.BloodDonorApp.entity.BloodTest;
+import gr.hua.dit.ds.BloodDonorApp.entity.Notification;
+import gr.hua.dit.ds.BloodDonorApp.entity.Role;
+import gr.hua.dit.ds.BloodDonorApp.entity.Hospital;
+import gr.hua.dit.ds.BloodDonorApp.entity.User;
+import gr.hua.dit.ds.BloodDonorApp.repository.*;
+import jakarta.annotation.PostConstruct;
+import jakarta.transaction.Transactional;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+@Service
+public class InitialDataService {
+
+    private static final int LAST_HOSPITAL_ID = 10;
+    private static final int LAST_NOTIFICATION_ID= 4;
+
+
+    private final UserRepository userRepository;
+
+    private final RoleRepository roleRepository;
+    private final HospitalRepository hospitalRepository;
+    private final ApplicationRepository applicationRepository;
+    private final BloodTestRepository bloodTestRepository;
+
+    private final NotificationRepository notificationRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public InitialDataService(UserRepository userRepository,
+                              RoleRepository roleRepository,
+                              HospitalRepository hospitalRepository,
+                              ApplicationRepository applicationRepository,
+                              BloodTestRepository bloodTestRepository,
+                              NotificationRepository notificationRepository,
+                              PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.hospitalRepository = hospitalRepository;
+        this.applicationRepository = applicationRepository;
+        this.bloodTestRepository = bloodTestRepository;
+        this.notificationRepository = notificationRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    private void createUsersAndRoles() {
+        final List<String> rolesToCreate = List.of("ROLE_ADMIN", "ROLE_DONOR", "ROLE_USER","ROLE_SECRETARY");
+        for (final String roleName : rolesToCreate) {
+            roleRepository.findByName(roleName).orElseGet(() -> {
+                roleRepository.save(new Role(roleName));
+                return null;
+            });
+        }
+
+        this.userRepository.findByUsername("user").orElseGet(() -> {
+            User user = new User("user", "user@hua.gr", this.passwordEncoder.encode("1234"));
+            Set<Role> roles = new HashSet<>();
+            roles.add(this.roleRepository.findByName("ROLE_USER").orElseThrow());
+            roles.add(this.roleRepository.findByName("ROLE_ADMIN").orElseThrow());
+            roles.add(this.roleRepository.findByName("ROLE_DONOR").orElseThrow());
+            user.setRoles(roles);
+            userRepository.save(user);
+            return null;
+        });
+        this.userRepository.findByUsername("secretary").orElseGet(() -> {
+            User user = new User("secretary", "secretary@hua.gr", this.passwordEncoder.encode("1234"));
+            Set<Role> roles = new HashSet<>();
+            roles.add(this.roleRepository.findByName("ROLE_USER").orElseThrow());
+            roles.add(this.roleRepository.findByName("ROLE_SECRETARY").orElseThrow());
+            user.setRoles(roles);
+            userRepository.save(user);
+            return null;
+        });
+        this.userRepository.findByUsername("admin").orElseGet(() -> {
+            User user = new User("admin", "admin@hua.gr", this.passwordEncoder.encode("1234"));
+            Set<Role> roles = new HashSet<>();
+            roles.add(this.roleRepository.findByName("ROLE_USER").orElseThrow());
+            roles.add(this.roleRepository.findByName("ROLE_SECRETARY").orElseThrow());
+            roles.add(this.roleRepository.findByName("ROLE_ADMIN").orElseThrow());
+            user.setRoles(roles);
+            userRepository.save(user);
+            return null;
+        });
+    }
+
+    private void createHospitals() {
+        for (int i=1; i<=LAST_HOSPITAL_ID; i++) {
+            final String name = "Hospital " + i;
+
+            this.hospitalRepository.findByName(name).orElseGet(() -> {
+                Hospital hospital = new Hospital();
+                hospital.setName(name);
+                this.hospitalRepository.save(hospital);
+                return null;
+            });
+        }
+    }
+
+
+
+    private void createApplications(){
+
+
+            User user = this.userRepository.findById(1).get();
+        this.applicationRepository.findByUser(user).orElseGet(() -> {
+            BloodTest bloodTest= new BloodTest(1,2,3,4,5,6,"1/2/3","agiosdimitrios");
+            Application application = new Application(13,"john","doe","O+","xalandri");
+            application.setBloodTest(bloodTest);
+            application.setUser(user);
+            userRepository.save(user);
+            applicationRepository.save(application);
+            return null;
+        });
+
+
+    }
+
+
+
+
+
+    private void createNotifications(){
+        User user = this.userRepository.findByUsername("user").get();
+        List<Notification> notifications = new ArrayList<>();
+        for (int i=1; i<=LAST_NOTIFICATION_ID; i++){
+            Notification notification = new Notification();
+            notification.setType("emergency "+ i);
+            notification.setHospitalName("hospital "+ i);
+            notification.setUser(user);
+
+            notifications.add(notification);
+
+        }
+        user.setNotifications(notifications);
+        this.userRepository.save(user);
+
+    }
+
+    @PostConstruct
+    public void setup() {
+        this.createUsersAndRoles();
+        this.createHospitals();
+        this.createNotifications();
+        this.createApplications();
+    }
+}
+
+
